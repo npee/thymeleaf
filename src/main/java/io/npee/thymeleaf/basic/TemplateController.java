7d0b21ed4d31137bdb75fd3e581353df35f6b492
npee <jmc4664@gmail.com>
2021-10-16 20:06:20 +0900

    타임리프 - 템플릿 조각

---
+++ src/main/java/io/npee/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,18 @@
+package io.npee.thymeleaf.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    @GetMapping("/fragment")
+    public String fragment() {
+
+        return "template/fragment/fragmentMain";
+    }
+}
