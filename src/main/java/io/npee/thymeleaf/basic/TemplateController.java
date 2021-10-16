8e20a59e629f9273ecf47cf65184efa60b5d135b
npee <jmc4664@gmail.com>
2021-10-16 20:16:00 +0900

    타임리프 - 레이아웃(head 수준)

--- src/main/java/io/npee/thymeleaf/basic/TemplateController.java
+++ src/main/java/io/npee/thymeleaf/basic/TemplateController.java
@@ -15,4 +15,10 @@ public class TemplateController {
 
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("/layout")
+    public String layout() {
+
+        return "template/layout/layoutMain";
+    }
 }
